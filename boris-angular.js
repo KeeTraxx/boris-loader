f8c331429dc0f284de42eaec0eb4ee747c1de58d
Khôi Tran <kt@compile.ch>
2015-06-09 14:08:17 +0200

    switching to angular

--- boris-angular.js
+++ boris-angular.js
@@ -1,13 +1,24 @@
 angular.module('boris-loader', [])
-    .controller('BorisController', [function(){
+    .controller('BorisController', [function () {
         console.log('ah sali');
     }])
-    .directive('boris', function () {
+    .directive('boris', function ($http, $window) {
         return {
-            link: function() {
+            link: function ($scope) {
                 console.log('ah sali');
+                var c = $window.angular.callbacks.counter.toString(36);
+                $window['angularcallbacks_' + c] = function (data) {
+                    $window.angular.callbacks['_' + c](data);
+                    delete $window['angularcallbacks_' + c];
+                };
+
+                $http.jsonp('http://boris.unibe.ch/cgi/exportview/divisions/DCD5A442BB9CE17DE0405C82790C4DE2/JSON/DCD5A442BB9CE17DE0405C82790C4DE2.js?callback=JSON_CALLBACK').success(function (data) {
+                    $scope.data = data;
+                }).error(function (err) {
+
+                });
             },
-            templateUrl: 'boristemplate'
+            template: '<pre>{{data | json}}</pre>'
         }
     });
 
